d454712586fdda3a8ef8f5e441669f15d3355d61
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-04-08 00:50:50 +0530

    Added bootstrap styles

--- diary-frontend/src/app/components/posts/posts.component.ts
+++ diary-frontend/src/app/components/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from 'src/app/services/posts.service';
+import { PostDB } from 'src/app/models/post.model';
 
 @Component({
   selector: 'app-posts',
@@ -10,10 +11,18 @@ export class PostsComponent implements OnInit {
 
   constructor(private postsService : PostsService) { }
 
+  postsData:PostDB[] = [{
+    _id:"Loading",
+    title:"Loading",
+    body:"Loading",
+    date:Date.now(),
+    userId:"Loading"
+  }]
+
   ngOnInit(): void {
     this.postsService.fetchPosts().subscribe(
       data => {
-        console.log(data)
+        this.postsData = data
       },
       err => {
         console.log(err)
